605739341084d95f0dc9820fd536f53ba24d766a
lifehaverdev <jfkwebdev@gmail.com>
2026-02-18 19:40:31 -0500

    feat: add Gallery Brutalism wallet button overrides
    CSS overrides for micro-web3 FloatingWalletButton:
    - Replace Temple of Capital theming with Gallery Brutalism
    - Monochrome color scheme (no gradients)
    - Clean typography (Helvetica Neue, monospace for address)
    - Minimal shadows and effects
    - Circular button when disconnected, pill when connected
    - Clean dropdown menu styling
    
    Created Web3 component wrapper for easy importing.

---
+++ src/components/Web3/WalletButton.js
@@ -0,0 +1,12 @@
+/**
+ * WalletButton - Floating wallet connection button
+ *
+ * Re-exports FloatingWalletButton from micro-web3
+ * Styled via Gallery Brutalism overrides in components-v2.css
+ *
+ * @example
+ * import { WalletButton } from './components/Web3/WalletButton.js';
+ * h(WalletButton, { walletService })
+ */
+
+export { FloatingWalletButton as WalletButton } from '@monygroupcorp/micro-web3';
